b6c59a65b17a63cbbd847b9323279765a849a073
frankool35 <efrank6@byupathway.edu>
2025-11-27 12:28:46 +0100

    discover_page1.html

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,50 @@
+export const places = [
+  {
+    "name": "National Museum Lagos",
+    "address": "Awolowo Road, Onikan, Lagos Island",
+    "image": "images/the-national-museum.webp",
+    "description": "A major cultural museum showcasing Nigeria’s history, traditional art, and archaeological collections."
+  },
+  {
+    "name": "Lekki Conservation Centre",
+    "address": "Km 19, Lekki-Epe Expressway, Lekki",
+    "image": "images/lekki-conservation.webp",
+    "description": "A serene nature reserve known for its canopy walk, wildlife, and peaceful outdoor environment."
+  },
+  {
+    "name": "Nike Art Gallery",
+    "address": "2 Elegushi Road, Lekki Phase 1",
+    "image": "images/nike-art-gallery.webp",
+    "description": "One of West Africa’s largest art galleries, featuring thousands of artworks by Nigerian artists."
+  },
+  {
+    "name": "Terra Kulture",
+    "address": "1376 Tiamiyu Savage St., Victoria Island",
+    "image": "images/terra-kulture.webp",
+    "description": "A cultural center promoting Nigerian arts, food, theater performances, and literary showcases."
+  },
+  {
+    "name": "Tarkwa Bay Beach",
+    "address": "Lagos Harbour, accessed by boat",
+    "image": "images/tarwa-bay.webp",
+    "description": "A family-friendly beach known for calm waters, swimming, and relaxation."
+  },
+  {
+    "name": "Eko Hotel & Suites",
+    "address": "1415 Adetokunbo Ademola St., Victoria Island",
+    "image": "images/eko_hotel_and _suites.webp",
+    "description": "A major hospitality hub with conference centers, luxury rooms, and business event facilities."
+  },
+  {
+    "name": "Balogun Market",
+    "address": "Broad Street Area, Lagos Island",
+    "image": "images/balogun-market.webp",
+    "description": "One of Nigeria’s busiest commercial hubs for textiles, electronics, fashion, and food items."
+  },
+  {
+    "name": "Freedom Park Lagos",
+    "address": "Old Prison Ground, Broad St., Lagos Island",
+    "image": "images/freedom_park.webp",
+    "description": "A memorial and leisure park built from a former colonial prison — used today for concerts and public events."
+  }
+];
